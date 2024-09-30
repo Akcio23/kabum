442390dee2c4e0d680ec000319c36c9cc4482a72
Akcio23 <dev.akcio@gmail.com>
2024-09-29 23:57:11 -0300

    url

--- src/backend/api/config.js
+++ src/backend/api/config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const blogFetch = axios.create({
-    baseURL: 'https://contato-kabum.vercel.app', // configurando URL global
+    baseURL: 'https://contato-kabum-cktcykm6m-akcios-projects.vercel.app', // configurando URL global
     headers:{
         'Content-Type': 'application/json' // configurando header padrão
     }
