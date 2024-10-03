3a52a0d3d37dc6e6b5ebf005c2ffb73bbc6317a3
Akcio23 <dev.akcio@gmail.com>
2024-10-02 22:49:04 -0300

    02/10

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-[100%] gap-3  bg-gradient-to-b from-orange-400 to-orange-600 rounded-t-3xl mt-3 ">
-        <p className='text-white'>Developed by: Pedro Acacio</p>
+    <div className="flex flex-col justify-center items-center w-[100%]  bg-gradient-to-b from-orange-400 to-orange-600 rounded-t-3xl mt-3 ">
+        <p className='text-white mt-2 '>Developed by: Pedro Acacio</p>
       
     </div>
   )
